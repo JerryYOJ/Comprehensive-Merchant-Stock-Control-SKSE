3438a73515a69c9c5e8be18e9a57103afb6a1866
jerry <jerry@test.com>
2025-08-04 09:21:13 +1200

    changed log level

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -10,7 +10,7 @@ void InitializeLog() {
     auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
     auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
     spdlog::set_default_logger(std::move(loggerPtr));
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_level(spdlog::level::trace);
     spdlog::flush_on(spdlog::level::debug);
 }
 
